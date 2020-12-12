2fca42a1286e9853174a0b78254eb206e53eb92e
Arpit <arpitmalik66@gmail.com>
2020-12-12 20:34:41 +0530

    structure components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
 
@@ -18,26 +18,28 @@ class DishDetail extends Component {
                     <div c>
                         <p>{comment.comment}</p>
                         <p>
-                            -- {comment.author}, {new Date(comment.date).toDateString()}
+                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         </p>
                     </div>
                 );
             });
 
             return (
-                <div className="row">
-                    <div key={this.props.dish.id} className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg  width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                                <CardTitle>{this.props.dish.name}</CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        {comments}
+                <div className="container">
+                    <div className="row">
+                        <div key={this.props.dish.id} className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg  width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                                <CardBody>
+                                    <CardTitle>{this.props.dish.name}</CardTitle>
+                                    <CardText>{this.props.dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            {comments}
+                        </div>
                     </div>
                 </div>
             );
